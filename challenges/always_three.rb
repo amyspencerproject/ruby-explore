12b8de17a05737da29471dc3a466ad0f5fbb440a
amyspencerproject <amy@spencerproject.com>
2022-05-11 09:39:27 -0600

    Learned basics of writing methods

--- challenges/always_three.rb
+++ challenges/always_three.rb
@@ -1,9 +1,36 @@
+# Refactor to be only two variables
+# puts "Give me a number!"
+# initial_number = gets.to_i
+# number = initial_number
+# number += 5
+# number *= 2
+# number -= 4
+# number /= 2
+# number = number - initial_number
+# puts "Always #{number}"
+
+# Refactor to be only 3 lines with one variable
+# puts "Give me a number!"
+# number = gets.to_i
+# # number = number - ((((number+5)*2)-4)/2)
+# # puts " #{number} "
+# puts "Always #{number = ((((number+5)*2)-4)/2) - number}"
+
+# Make this into a method
+# def always_three
+#     puts "Give me a number!"
+#     final_number = gets.to_i
+#     puts "Wow! Always #{final_number = ((((final_number+5)*2)-4)/2) - final_number}"
+# end
+
+# always_three
+
+# Make this into a method with arguments
+def always_three(final_number)
+    final_number = ((((final_number+5)*2)-4)/2) - final_number
+end
+
 puts "Give me a number!"
-number = gets
-number1 = number.to_i
-number1 += 5
-number1 *= 2
-number1 -= 4
-number1 /= 2
-final_number = number1 - number.to_i
-puts "#{final_number}"
+final_number = gets.to_i
+
+puts "Wow! Always " + always_three(final_number).to_s
---
+++ challenges/my_first_method.rb
@@ -0,0 +1,22 @@
+#define and end your method
+# def my_first_method
+#     puts "Hooray! This is my first method!!"
+# end
+
+# my_first_method
+
+# def simple_math
+#     puts "Give me a number"
+#     number=gets
+#     puts "My second method! Your number plus two equals #{number = number.to_i + 2}"
+# end
+
+# simple_math
+
+def simple_math (num1, num2)
+    sum= num1 + num2
+    puts "The sum of #{num1} and #{num2} equals #{sum}"
+end
+
+simple_math(3,4)
+
