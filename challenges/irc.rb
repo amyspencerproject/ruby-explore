7a7e357c17aaa69e0653fb0a8012054ce3c4c9ac
amyspencerproject <amy@spencerproject.com>
2022-05-27 21:08:36 -0600

    Added if statements for message bot script

--- challenges/irc.rb
+++ challenges/irc.rb
@@ -5,9 +5,12 @@ port    = 6667
 socket  = TCPSocket.open(server, port)
  
 nickname = 'SkillcrushBotOMG'
+channel = '#snowrocks'
  
 socket.puts "NICK #{nickname}"
 socket.puts "USER #{nickname} 0 * #{nickname}"
+socket.puts "JOIN #{channel}"
+socket.puts "PRIVMSG #{channel} :OMG You are not alone! Sono qui!!"
  
 while message = socket.gets do
  
@@ -20,5 +23,13 @@ while message = socket.gets do
         puts "PONG #{server}"
         
         socket.puts "PONG #{server}"
+        
+    elsif message.match('How are you')
+        puts "PRIVMSG #{channel} : OMG I am doing great"
+        socket.puts "PRIVMSG #{channel} : OMG I am doing great"
+    end
+
+    if message.match('Che cosa fai')
+        socket.puts "PRIVMSG #{channel} : OMG Sto leggendo le notizie! Tutto e cattivo!"
     end
 end
